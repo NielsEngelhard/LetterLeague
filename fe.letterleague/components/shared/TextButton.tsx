aa0b74ccb4efdc22b88c265798ebaed9521c7cb2
Niels Engelhard <niels@robinhq.com>
2025-04-03 21:52:43 +0200

    texbutton

--- fe.letterleague/components/shared/TextButton.tsx
+++ fe.letterleague/components/shared/TextButton.tsx
@@ -1,12 +1,19 @@
 interface Props {
     text: string;
     handleClick?: () => void;
+    colorTheme: 1 | 2 | 3 | 4;
 }
 
-export default function TextButton({ text, handleClick}: Props) {
+export default function TextButton({ text, handleClick, colorTheme = 1}: Props) {
+    const colorThemeClasses: Record<number, string> = {
+        1: 'iconic-text-one',
+        2: 'iconic-text-two'
+    };
+
     return (
-        <div className="iconic-text-one text-lg">
+        <button className={`${colorThemeClasses[colorTheme]} text-lg`}
+                onClick={handleClick}>
             {text}
-        </div>
+        </button>
     );
 }
